5fa3d8516826a0dcf39710b0b8ff57c32010a487
ryou <einryou@gmail.com>
2020-07-11 19:48:09 +0900

    feat: ワールド最大人数に関する文章を調整

--- src/App/localComponents/AboutCapacity/index.vue
+++ src/App/localComponents/AboutCapacity/index.vue
@@ -6,14 +6,16 @@
     </button>
     <Dialog v-if="dialogIsVisible" title="インスタンスの最大人数に関して">
       <template v-slot:content>
-        インスタンスの現在人数及び最大人数は、「現在人数 /
-        最大人数」という形でワールドのサムネイル画像の右肩に表記しています。<br />
-        <br />
-        インスタンスには、ワールドのcapacityの2倍の人数が実際には入れます。（例外あり）<br />
-        なので、最大人数は以下の基準で算出し表記しています。<br />
-        <br />
-        ・ワールドのcapacityの2倍<br />
-        ・例外として、capacityが1の場合は2倍しない（1のまま）
+        <MarkdownText
+          :markdownText="
+            `インスタンスの現在人数及び最大人数は、「現在人数 / 最大人数」という形でインスタンスのサムネイル右肩に表記しています。
+
+インスタンスには、ワールドのcapacityの2倍の人数が実際には入れます。（例外として、capacityが1の場合は1人しか入れません）
+
+表記している最大人数は、インスタンスに実際入れる人数を上の基準で計算の上表記しています。
+`
+          "
+        />
       </template>
       <template v-slot:buttonArea>
         <Button @click="hideDialog">閉じる</Button>
--- src/App/localComponents/AboutCapacity/script.ts
+++ src/App/localComponents/AboutCapacity/script.ts
@@ -3,12 +3,14 @@ import Vue from 'vue'
 import Dialog from '@/components/Dialog/index.vue'
 import Icon from '@/components/Icon/index.vue'
 import Button from '@/components/Button/index.vue'
+import MarkdownText from '@/components/MarkdownText/index.vue'
 
 @Component({
   components: {
     Dialog,
     Icon,
     Button,
+    MarkdownText,
   },
 })
 export default class AboutCapacity extends Vue {

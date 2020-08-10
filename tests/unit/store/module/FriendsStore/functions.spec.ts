ab7760e509fc52eaeba90d9e214d1125446878d1
ryou <einryou@gmail.com>
2020-08-10 16:54:05 +0900

    FriendsStoreのテストを記述

---
+++ tests/unit/store/module/FriendsStore/functions.spec.ts
@@ -0,0 +1,66 @@
+import { FetchFriendsFunction } from '@/infras/network/vrcApi'
+import { fetchAllFriends } from '@/presentations/store/module/FriendsStore/functions'
+
+describe('fetchAllFriends', () => {
+  const dummyFriend = {
+    id: 'dummy',
+    username: 'dummy',
+    displayName: 'dummy',
+    currentAvatarImageUrl: 'dummy',
+    currentAvatarThumbnailImageUrl: 'dummy',
+    location: 'dummy',
+  }
+  const mockFetchFriends: FetchFriendsFunction = async page => {
+    if (page === 0) {
+      return [
+        {
+          ...dummyFriend,
+          id: 'usr_0',
+        },
+      ]
+    }
+    if (page === 1) {
+      return [
+        {
+          ...dummyFriend,
+          id: 'usr_1',
+        },
+      ]
+    }
+    if (page === 2) {
+      return [
+        {
+          ...dummyFriend,
+          id: 'usr_1',
+        },
+      ]
+    }
+    if (page === 3) {
+      return [
+        {
+          ...dummyFriend,
+          id: 'usr_2',
+        },
+      ]
+    }
+    return []
+  }
+
+  it('全ユーザーを取得し、重複は除去される', async () => {
+    const result = await fetchAllFriends(mockFetchFriends)
+    expect(result).toEqual([
+      {
+        ...dummyFriend,
+        id: 'usr_0',
+      },
+      {
+        ...dummyFriend,
+        id: 'usr_1',
+      },
+      {
+        ...dummyFriend,
+        id: 'usr_2',
+      },
+    ])
+  })
+})

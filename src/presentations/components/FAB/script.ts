045ce92a2a59381ba3c3eeecd0db2b11740267eb
ryou <einryou@gmail.com>
2020-08-11 16:20:31 +0900

    feat: ざっくりレイアウトSP対応

--- src/presentations/components/FAB/script.ts
+++ src/presentations/components/FAB/script.ts
@@ -1,8 +1,18 @@
-import { Component } from 'vue-property-decorator'
+import { Component, Prop } from 'vue-property-decorator'
 import Vue from 'vue'
 
 @Component
 export default class FAB extends Vue {
+  @Prop({ default: 'right' })
+  position!: string
+
+  @Prop({ default: 'white' })
+  color!: string
+
+  get rootClass() {
+    return [`-${this.position}`, `-${this.color}`]
+  }
+
   onClick() {
     this.$emit('click')
   }
--- src/presentations/views/Home/index.vue
+++ src/presentations/views/Home/index.vue
@@ -1,10 +1,13 @@
 <template>
   <div class="c-home">
-    <div class="side">
-      <div v-if="showOnlineFriendsListLoading" class="u-alignCenter u-pt20">
-        <g-Spinner color="black" :size="24" />
+    <div class="side" :class="{ '-visible': isVisibleSideMenu }">
+      <div class="overlay" @click="hideInstanceList" />
+      <div class="content">
+        <div v-if="showOnlineFriendsListLoading" class="u-alignCenter u-pt20">
+          <g-Spinner color="black" :size="24" />
+        </div>
+        <OnlineFriendsList v-else :friends="friends" />
       </div>
-      <OnlineFriendsList v-else :friends="friends" />
     </div>
     <div class="main">
       <div v-if="showInstanceListLoading" class="u-alignCenter u-pt40">
@@ -12,7 +15,12 @@
       </div>
       <template v-else>
         <InstanceList :instances="instances" />
-        <g-FAB @click="reload">
+        <div class="u-spOnly">
+          <g-FAB position="left" @click="showInstanceList">
+            <g-Icon :size="50" color="black">people</g-Icon>
+          </g-FAB>
+        </div>
+        <g-FAB color="green" @click="reload">
           <g-Spinner v-if="showFABLoading" color="white" />
           <g-Icon v-else :size="50" color="white">refresh</g-Icon>
         </g-FAB>

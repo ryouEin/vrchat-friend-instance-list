8a0a73186ca52ccc5ebfa2b82a10e373d2a764d0
ryou <einryou@gmail.com>
2020-08-20 18:32:30 +0900

    feat: LocationからオーナーIDを算出する関数の作成

---
+++ src/shame/getOwnerIdFromLocation.ts
@@ -0,0 +1,15 @@
+/*
+どこか適切な場所に置きたい
+ */
+import { InstanceLocation } from '@/types'
+
+export function getOwnerIdFromLocation(
+  location: InstanceLocation
+): string | undefined {
+  const regExResult = /\(.*?\)/.exec(location)
+  if (regExResult === null) {
+    return undefined
+  }
+
+  return regExResult[0].replace('(', '').replace(')', '')
+}

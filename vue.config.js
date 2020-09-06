ef726790e6fdf08ab37597feb04b91566bb3b549
ryou <einryou@gmail.com>
2020-09-06 15:01:31 +0900

    refactor: ESLintの設定見直し

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,12 @@
-// TODO: 設定型ファイルのJSのLintどうするか再考
-/* eslint-disable */
 const path = require('path')
 
 module.exports = {
   pluginOptions: {
     'style-resources-loader': {
       preProcessor: 'scss',
-      patterns: [path.resolve(__dirname, './src/presentations/stylesheets/init.scss')],
+      patterns: [
+        path.resolve(__dirname, './src/presentations/stylesheets/init.scss'),
+      ],
     },
   },
 }

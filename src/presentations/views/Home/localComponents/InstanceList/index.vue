9006df4cfbf08d36e15ff0d81b50f0c2ae864fe1
ryou <einryou@gmail.com>
2020-09-16 22:56:42 +0900

    feat: コンテンツの初期表示時にフェードで表示するように

--- src/presentations/views/Home/localComponents/InstanceList/index.vue
+++ src/presentations/views/Home/localComponents/InstanceList/index.vue
@@ -1,46 +1,48 @@
 <template>
-  <div class="c-instanceList">
-    <DynamicScroller
-      id="InstanceList"
-      :items="items"
-      :min-item-size="250"
-      class="scroller"
-      :buffer="1500"
-      ref="scroller"
-    >
-      <template #before>
-        <div class="filterArea">
-          <div class="toggleButtonGroup">
-            <div class="toggleButtonGroup_toggle">
-              <g-Toggle v-model="showOnlyFavoriteFriends" />
-            </div>
-            <div class="toggleButtonGroup_label">
-              Favoriteユーザのみ表示
+  <transition name="t-fade" appear>
+    <div class="c-instanceList">
+      <DynamicScroller
+        id="InstanceList"
+        :items="items"
+        :min-item-size="250"
+        class="scroller"
+        :buffer="1500"
+        ref="scroller"
+      >
+        <template #before>
+          <div class="filterArea">
+            <div class="toggleButtonGroup">
+              <div class="toggleButtonGroup_toggle">
+                <g-Toggle v-model="showOnlyFavoriteFriends" />
+              </div>
+              <div class="toggleButtonGroup_label">
+                Favoriteユーザのみ表示
+              </div>
             </div>
           </div>
-        </div>
-      </template>
-      <template v-slot="{ item, index, active }">
-        <DynamicScrollerItem
-          :item="item"
-          :active="active"
-          :size-dependencies="[item.friends.length]"
-          :data-index="index"
-        >
-          <div class="item">
-            <InstanceListItem
-              :instance="item.instance"
-              :friends="item.friends"
-            />
-          </div>
-        </DynamicScrollerItem>
-      </template>
-    </DynamicScroller>
-    <WatchInstanceDialog />
-    <div v-if="isInitialized" class="toTop">
-      <g-ToTopButton :scrollerElement="scrollerElement" />
+        </template>
+        <template v-slot="{ item, index, active }">
+          <DynamicScrollerItem
+            :item="item"
+            :active="active"
+            :size-dependencies="[item.friends.length]"
+            :data-index="index"
+          >
+            <div class="item">
+              <InstanceListItem
+                :instance="item.instance"
+                :friends="item.friends"
+              />
+            </div>
+          </DynamicScrollerItem>
+        </template>
+      </DynamicScroller>
+      <WatchInstanceDialog />
+      <div v-if="isInitialized" class="toTop">
+        <g-ToTopButton :scrollerElement="scrollerElement" />
+      </div>
     </div>
-  </div>
+  </transition>
 </template>
 
 <script lang="ts" src="./script.ts" />
--- src/presentations/views/Home/localComponents/OnlineFriendsList/index.vue
+++ src/presentations/views/Home/localComponents/OnlineFriendsList/index.vue
@@ -1,29 +1,31 @@
 <template>
-  <div class="c-userList">
-    <RecycleScroller
-      class="scroller"
-      :items="sortedFriends"
-      :item-size="itemHeight"
-      key-field="id"
-      :buffer="1500"
-      ref="scroller"
-    >
-      <template v-slot="{ item }">
-        <div class="item">
-          <OnlineFriendsListItem
-            :friend="item"
-            :style="{ height: `${itemHeight}px` }"
-          />
-        </div>
-      </template>
-      <template #after>
-        <div class="lastSpacer" />
-      </template>
-    </RecycleScroller>
-    <div v-if="isInitialized" class="toTop">
-      <g-ToTopButton :scrollerElement="scrollerElement" />
+  <transition name="t-fade" appear>
+    <div class="c-userList">
+      <RecycleScroller
+        class="scroller"
+        :items="sortedFriends"
+        :item-size="itemHeight"
+        key-field="id"
+        :buffer="1500"
+        ref="scroller"
+      >
+        <template v-slot="{ item }">
+          <div class="item">
+            <OnlineFriendsListItem
+              :friend="item"
+              :style="{ height: `${itemHeight}px` }"
+            />
+          </div>
+        </template>
+        <template #after>
+          <div class="lastSpacer" />
+        </template>
+      </RecycleScroller>
+      <div v-if="isInitialized" class="toTop">
+        <g-ToTopButton :scrollerElement="scrollerElement" />
+      </div>
     </div>
-  </div>
+  </transition>
 </template>
 
 <script lang="ts" src="./script.ts" />

5b7e953ae17196081a030f23e48de6c9b6d7eef7
ryou <einryou@gmail.com>
2020-10-02 19:15:56 +0900

    doc: コメント追加

--- src/presentations/components/Button/index.tsx
+++ src/presentations/components/Button/index.tsx
@@ -1,3 +1,13 @@
+/*
+Vueでのテンプレート型チェックに関して
+TSX対応すれば大枠出来るが以下に注意
+・クラススタイルでコンポーネントを書く必要がある
+  ・`implements Props` と書くことでPropsが実装されていることを担保するため
+  ・`implements Props` と書かなくてもコンパイルエラーが出ないことに注意が必要
+  ・また、Propデコレータをつけ忘れてる場合もエラーが出ない
+・Eventは使えない
+  ・Reactみたいな感じで、親から関数をPropsとして渡してもらって、それを実行する形になる
+ */
 import * as tsx from 'vue-tsx-support'
 import { Component, Prop } from 'vue-property-decorator'
 import styles from './style.module.scss'
@@ -12,10 +22,9 @@ interface ComponentEvents {
 }
 
 @Component
-export default class Button extends tsx.Component<
-  ComponentProps,
-  ComponentEvents
-> {
+export default class Button
+  extends tsx.Component<ComponentProps, ComponentEvents>
+  implements ComponentProps {
   @Prop({ type: Boolean, default: false })
   readonly primary!: boolean
 

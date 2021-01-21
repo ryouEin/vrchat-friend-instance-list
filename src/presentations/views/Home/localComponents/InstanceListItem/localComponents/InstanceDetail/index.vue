2a2e5d76c1e5aa7bcaf5569d48e56d31883371eb
ryou <einryou@gmail.com>
2021-01-21 23:46:50 +0900

    refactor: InstanceDetailからCanJoinな状況を抜き出した

--- src/presentations/views/Home/localComponents/InstanceListItem/localComponents/InstanceDetail/index.vue
+++ src/presentations/views/Home/localComponents/InstanceListItem/localComponents/InstanceDetail/index.vue
@@ -3,16 +3,8 @@
     <div v-if="isPrivate" class="inner -private">
       <div class="textOnlyTitle">Private</div>
     </div>
-    <div v-else-if="world !== null" class="inner">
-      <WorldInfo :instance="instance" :world="world" />
-    </div>
-    <div v-else-if="isLoading" class="inner">
-      <div class="loading">
-        <g-Spinner color="white" />
-      </div>
-    </div>
-    <div v-else class="inner -error">
-      <div class="textOnlyTitle">Error</div>
+    <div v-else class="inner">
+      <CanJoinInstanceDetail :instance="instance" />
     </div>
   </div>
 </template>
--- src/presentations/views/Home/localComponents/InstanceListItem/localComponents/InstanceDetail/script.ts
+++ src/presentations/views/Home/localComponents/InstanceListItem/localComponents/InstanceDetail/script.ts
@@ -3,49 +3,19 @@ import Vue from 'vue'
 import { Instance, World } from '@/types'
 import WorldInfo from '@/presentations/views/Home/localComponents/InstanceListItem/localComponents/WorldInfo/index.vue'
 import { FETCH_WORLD, FetchWorld } from '@/presentations/views/Home/injectInfo'
+import CanJoinInstanceDetail from '@/presentations/views/Home/localComponents/InstanceListItem/localComponents/InstanceDetail/CanJoinInstanceDetail.vue'
 
-// TODO: このコンポーネント、更にPrivateか否かでわけたい（worldに関する処理がPrivateには不要なので、現状凝集度が低い）
 @Component({
   components: {
     WorldInfo,
+    CanJoinInstanceDetail,
   },
 })
 export default class InstanceDetail extends Vue {
-  @Inject(FETCH_WORLD)
-  private readonly fetchWorld!: FetchWorld
-
   @Prop()
   readonly instance?: Instance
 
-  private isLoading = false
-
-  private world: World | null = null
-
   get isPrivate(): boolean {
     return this.instance === undefined
-  }
-
-  // virtual-scrollerはコンポーネントを使い回すためlocationの変更を見て
-  // 初期化する必要がある
-  @Watch('instance.location')
-  async onChangeLocation() {
-    await this.init()
-  }
-
-  async init() {
-    this.isLoading = true
-    this.world = null
-    if (!this.isPrivate) {
-      this.world = await this.fetchWorld(
-        // !this.isPrivateでthis.instanceがundefinedでないことは担保されているので
-        this.instance!.worldId
-      ).finally(() => {
-        this.isLoading = false
-      })
-    }
-  }
-
-  async created() {
-    await this.init()
   }
 }

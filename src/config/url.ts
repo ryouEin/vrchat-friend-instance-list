12ec081cf89c404e325a05bd4d829c9a67a08c04
ryou <einryou@gmail.com>
2022-08-11 17:50:20 +0900

    APIの仕様変更に伴い、invite meが動作しなくなっていた不具合の修正

--- src/config/url.ts
+++ src/config/url.ts
@@ -13,7 +13,7 @@ export const VrcApiUrl = {
     VRC_API_URL + `/api/1/worlds/${worldId}`,
   getFetchWorldsUrl: () => VRC_API_URL + `/api/1/worlds`,
   getInviteMeUrl: (location: InstanceLocation) =>
-    VRC_API_URL + `/api/1/instances/${location}/invite`,
+    VRC_API_URL + `/api/1/invite/myself/to/${location}`,
 }
 
 export const VRCHAT_WEBSITE_URL = 'https://vrchat.com/login'
